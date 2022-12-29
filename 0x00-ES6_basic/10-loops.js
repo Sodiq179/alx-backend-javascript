abe0ff6da5534fd9b728fba9ed54cba9c9eb1351
Sodiq179 <babawalesodiq996@gmail.com>
2022-12-29 19:40:14 +0100

    Initial commit

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,4 +1,4 @@
-function appendToEachArrayValue(array, appendString) {
+export default function appendToEachArrayValue(array, appendString) {
   const array2 = [];
   for (const idx of array) {
     const value = appendString + idx;
